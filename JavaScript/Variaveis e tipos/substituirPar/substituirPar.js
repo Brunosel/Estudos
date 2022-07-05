ed255b9e88dad1691684cfb0199eaf224252e14f
Brunosel <brunosel@hotmail.com>
2022-07-04 22:20:37 -0300

    substituir par done

--- JavaScript/Variaveis e tipos/substituirPar/substituirPar.js
+++ JavaScript/Variaveis e tipos/substituirPar/substituirPar.js
@@ -1,1 +1,15 @@
-let
+let array = [1, 3, 4, 6, 80, 33, 23, 90];
+
+function substituirpares() {
+    if (!array.length) {
+        console.log('-1');
+    }
+    for (i = 0, x = array.length; i < x;i++) {
+        if (array[i] % 2 === 0) {
+            array[i] = 0;
+        }
+        console.log(array[i]);
+    }
+}
+
+substituirpares();
