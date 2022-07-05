989a9664bba14c5eb8ca4988a57d33e45a26d541
Brunosel <brunosel@hotmail.com>
2022-07-05 16:44:17 -0300

    for e while done

---
+++ JavaScript/for e while/for.js
@@ -0,0 +1,40 @@
+/*function multiplacadorPorDois(array) {
+    let multiplacados = [];
+
+    for (let i = 0, x = array.lenght; i < x; i++) {
+        multiplacados.push(array[i] * 2);
+    }
+
+    return multiplacados;
+}
+
+const meusNumeros = [2, 33, 456, 356, 40];
+
+multiplacadorPorDois(meusNumeros);*/
+
+// for in 
+
+function forInExemplo(obj) {
+    for (prop in obj) {
+        console.log(prop); // nome, idade, cidade
+        console.log(obj[prop]); // Joao, 20, Salvador
+    }
+}
+
+const objeto = {
+    nome: 'Joao',
+    idade: 20,
+    cidade: 'Salvador'
+}
+
+forInExemplo(objeto);
+
+// for of
+
+function forOfExemplo(nums) {
+    for (num of nums) {
+        console.log(num);
+    }
+}
+
+const nums = [1, 2, 3, 4, 5, 6];
---
+++ JavaScript/for e while/while.js
@@ -0,0 +1,16 @@
+(function exemploWhile() {
+    let i = 0;
+
+    while (i < 5) {
+        console.log(i);
+        i++;
+    }
+})();
+
+(function ExemploDoWhile() {
+    let i = 5;
+    do {
+        console.log(i);
+        i++;
+    } while(i < 5)
+})();
