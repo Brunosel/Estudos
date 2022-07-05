103d7c0417f57d0b1390f4d1e8903d415c1e59d8
Brunosel <brunosel@hotmail.com>
2022-07-04 23:17:18 -0300

    modulos done

---
+++ JavaScript/Modulos/funcoes.mjs
@@ -0,0 +1,17 @@
+function mostraIdade(nome, idade) {
+    return `A idade da ${nome} e ${idade}`;
+}
+
+function mostraCidade(nome, cidade) {
+    return `A cidade da ${nome} e ${cidade}`;
+}
+
+function mostraHobby(nome, hobby) {
+    return `O hobby da ${nome} e ${hobby}`;
+}
+
+export {
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+}
---
+++ JavaScript/Modulos/main.mjs
@@ -0,0 +1,14 @@
+import {
+    mostraIdade,
+    mostraCidade,
+    mostraHobby
+} from  './funcoes.mjs'
+
+console.log(mostraIdade('pedro', 18));
+console.log(mostraCidade('julia', 'campinas'));
+console.log(mostraHobby('leticia', 'leitura'));
+
+
+
+
+
