3713f61857309367defb6fa6fbebba642ef84223
Brunosel <brunosel@hotmail.com>
2022-07-20 11:00:42 -0300

    Update Set.js

---
+++ JavaScript/Mapas e conjuntos/Estrutura, método, Map vs arrays/Set.js
@@ -0,0 +1,12 @@
+const mySet = new Set(); // sets sao estruturar que armazenam apenas valores unicos
+
+mySet.add(1);
+mySet.add(5);
+
+mySet.has(1);
+// True
+
+mySet.has(3);
+// False
+
+mySet.delete(5);
